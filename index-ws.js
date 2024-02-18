bc6ad954bb4fec38a0bb45ea6448a1d8367c9788
gabie <gabriellamondvay@gmail.com>
2024-02-18 12:12:19 +0100

    correct error in sigint logic

--- index-ws.js
+++ index-ws.js
@@ -4,54 +4,83 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', function(req, res) {
-  res.sendFile('index.html', {root: __dirname});
+    res.sendFile('index.html', {root: __dirname});
 });
 
 server.on('request', app);
-
 server.listen(PORT, function () { console.log('Listening on ' + PORT); });
 
-// Begin Websocket
+
+
+
+/** Begin websocket */
 const WebSocketServer = require('ws').Server;
 
 // Attach websocket to express server
 const wss = new WebSocketServer({ server: server });
 
+process.on('SIGINT', () => {
+    console.log('sigint');
+    wss.clients.forEach(function each(client) {
+        client.close();
+    });
+    server.close(() => {
+        shutdownDB();
+    })
+})
 // Callback function takes the ws library as an argument
 wss.on('connection', function connection(ws) {
-
-  // Currently connected clients
   const numClients = wss.clients.size;
   console.log('Clients connected: ', numClients);
 
-  // Broadcast sends a message to everyone connected, function is defined at bottom of the file
   wss.broadcast(`Current visitors: ${numClients}`);
 
-  // Send message to connected client when socket state equals open
   if (ws.readyState === ws.OPEN) {
     ws.send('Welcome!');
   }
 
-  // Send broadcast when socket state equals 
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES(${numClients}, datetime('now'))
+  `);
+
   ws.on('close', function close() {
     wss.broadcast(`Current visitors: ${wss.clients.size}`);
     console.log('A client has disconnected');
   });
 
-  ws.on('error', function error() {
-    
-  });
 });
 
-/**
- * Broadcast data to all connected clients
- * @param  {Object} data
- * @void
- */
 wss.broadcast = function broadcast(data) {
   console.log('Broadcasting: ', data);
   wss.clients.forEach(function each(client) {
     client.send(data);
   });
 };
-/** End Websocket **/
+
+/* End Websocket */
+/* Begin database */
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `)
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    });
+}
+
+function shutdownDB() {
+    console.log('Shutting down db');
+
+    getCounts();
+    db.close();
+
+}
